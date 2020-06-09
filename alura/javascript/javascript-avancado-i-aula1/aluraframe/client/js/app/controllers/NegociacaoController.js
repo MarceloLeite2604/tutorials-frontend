b09c09b5121a2f841a52c6b7928193aab868d45d
MarceloLeite2604 <marceloleite2604@gmail.com>
2020-06-09 16:41:13 -0300

    Add content developed on Alura's second Javascript course

---
+++ alura/javascript/javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,42 @@
+class NegociacaoController {
+
+    constructor() {
+        /* Javascript allow us to associate a function to a variable, so we'll create a variable "$" and associate the "document" variable "querySelector" method to it. Problem is, since the method uses a "this" inside it, Javascript will use "$" variable as context execution. To avoid it, we use the method "bind" to define its scope as the original "document" variable. (tricky, isn't it?) */
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+
+        this._negociacoesView.update(this._listaNegociacoes);
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagemView.update(this._mensagem);
+    }
+
+    adiciona(event) {
+        event.preventDefault();
+
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._listaNegociacoes);
+        this._mensagem.texto = 'Negociação adicionada com sucesso.';
+        this._mensagemView.update(this._mensagem);
+        this._limpaFormulario();
+    }
+
+    _criaNegociacao() {
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+    }
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0;
+
+        this._inputData.focus();
+    }
+}
